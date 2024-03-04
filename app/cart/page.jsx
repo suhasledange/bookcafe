456a099f74f34d8b344aceba456d71d86a566fa8
Suhas Ledange <103985325+suhasledange@users.noreply.github.com>
2024-03-04 08:11:10 +0530

    Update page.jsx

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,12 +1,11 @@
 'use client'
-import Container from './Container'
 const Cart = () => {
   
 
   return (
-    <Container className="md:px-0 px-3 max-w-screen-xl mt-10 mb-10">
+    <div className="md:px-0 px-3 max-w-screen-xl mt-10 mb-10">
    cart
-    </Container>
+    </div>
 );
 }
 
