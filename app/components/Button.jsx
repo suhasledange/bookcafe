ccc6fb7d85f33e42c8e62fa4ec5c0ca213233a8f
suhasledange <suhasledange10@gmail.com>
2024-03-05 08:16:59 +0530

    img

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -5,5 +5,4 @@ const Button = ({text,className}) => {
     <button className={`transition-transform active:scale-95 hover:bg-black/[0.8] duration-150 bg-black text-white p-[0.3rem] px-3 tracking-wider ${className || ""}`}>{text}</button>
   )
 }
-
 export default Button
