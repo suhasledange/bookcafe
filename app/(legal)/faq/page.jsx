c1e9a0896532344a8518cf94fb5e7a0e0874dccb
suhasledange <suhasledange10@gmail.com>
2024-04-12 17:06:24 +0530

    footer

---
+++ app/(legal)/faq/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      faq
+    </div>
+  )
+}
+
+export default page
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,131 +1,140 @@
-'use client'
-import Link from "next/link"
-import Container from "./Container"
+"use client";
+import Link from "next/link";
+import Container from "./Container";
 import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
-
+import { IoLogoWhatsapp } from "react-icons/io";
 const Footer = () => {
+  const links = [
+    { id: 1, logo: <FaFacebookF size={20} />, link: "https://facebook.com" },
+    { id: 3, logo: <FaTwitter size={20} />, link: "https://twitter.com" },
+    { id: 2, logo: <FaYoutube size={20} />, link: "https://youtube.com" },
+    { id: 4, logo: <FaInstagram size={20} />, link: "https://instagram.com" },
+  ];
+
+  const info = [
+    { id: 1, text: "About Us", link: "/about" },
+    { id: 2, text: "Contact Us", link: "/contact" },
+    { id: 3, text: "Privacy Policy", link: "/privacy" },
+    { id: 4, text: "Terms And Conditions", link: "/terms" },
+    { id: 5, text: "Refund And Cancellation Policy", link: "cancellation" },
+  ];
+
+  const more = [
+    {id:1,text:'Browse Books',link:'/book'},
+    {id:2,text:'My Account',link:'/profile'},
+    {id:3,text:'Renting Guide',link:'/guide'},
+    {id:4,text:'FAQ',link:'/faq'},
+
+
+  ]
+
   return (
-    <footer className="bg-black text-white pt-14 pb-3 mt-16">
-            <Container className="flex justify-between flex-col md:flex-row gap-[50px] md:gap-0 max-w-screen-xl px-3 md:px-0">
-                {/* LEFT START */}
-                <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row">
-                    {/* MENU START */}
-                    <div className="flex flex-col gap-3 shrink-0">
-                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
-                            Find a store
-                        </div>
-                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
-                            become a partner
-                        </div>
-                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
-                            sign up for email
-                        </div>
-                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
-                            send us feedback
-                        </div>
-                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
-                            student discount
-                        </div>
-                    </div>
-                    {/* MENU END */}
+      <footer className="bg-black text-white pt-14 pb-3 mt-16">
 
-                    {/* NORMAL MENU START */}
-                    <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] shrink-0">
-                        {/* MENU START */}
-                        <div className="flex flex-col gap-3">
-                            <div className="font-oswald font-medium uppercase text-sm">
-                                get help
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Order Status
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Delivery
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Returns
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Payment Options
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Contact Us
-                            </div>
-                        </div>
-                        {/* MENU END */}
+        <Container className="flex justify-between flex-col md:flex-row gap-[50px] md:gap-0 max-w-screen-xl px-3 md:px-0">
 
-                        {/* MENU START */}
-                        <div className="flex flex-col gap-3">
-                            <div className="font-oswald font-medium uppercase text-sm">
-                                About
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                News
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Careers
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Investors
-                            </div>
-                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                                Sustainability
-                            </div>
-                        </div>
-                        {/* MENU END */}
-                    </div>
-                    {/* NORMAL MENU END */}
-                </div>
-                {/* LEFT END */}
+          <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row">
 
-                {/* RIGHT START */}
-                <div className="flex gap-4 justify-center md:justify-start">
-                    <div
-                        onClick={() =>
-                            window.open("https://facebook.com", "_blank")
-                        }
-                        className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
-                    >
-                        <FaFacebookF size={20} />
-                    </div>
-                    <Link
-                        href="https://twitter.com"
-                        className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
-                    >
-                        <FaTwitter size={20} />
-                    </Link>
-                    <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                        <FaYoutube size={20} />
-                    </div>
-                    <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                        <FaInstagram size={20} />
-                    </div>
-                </div>
-                {/* RIGHT END */}
-            </Container>
-            <Container className="mt-10">
+            <div className="flex flex-col gap-3 shrink-0">
+              <Link href="/" className="text-xl text-white font-bold">BookCafe</Link>
+              <div className="text-sm font-light text-white/[0.5]">
+                <p>Welcome to BookCafe!</p>
+                <p className=" w-[70%]">
+                  Your go-to destination for all your book rental needs.
+                </p>
+              </div>
+              <div className="mb-5 text-white/[0.5]">
+                <p> bookcafe30@gmail.com </p>
+              </div>
 
-                <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center">
-                    © 2024 BookCafe, Inc. All Rights Reserved
+              <div className="flex gap-4 justify-start">
+                {links.map((l) => (
+                  <Link
+                    key={l.id}
+                    href={l.link}
+                    className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer"
+                  >
+                    {l.logo}
+                  </Link>
+                ))}
+              </div>
+
+            </div>
+
+            <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] shrink-0">
+              <div className="flex flex-col gap-3">
+                <div className="font-oswald font-medium uppercase text-sm">
+                  Out Information
                 </div>
 
-                {/* <div className="flex gap-2 md:gap-5 text-center md:text-left flex-wrap justify-center">
-                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
-                        Guides
-                    </div>
-                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
-                        Terms of Sale
-                    </div>
-                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
-                        Terms of Use
-                    </div>
-                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
-                        Privacy Policy
-                    </div>
-                </div> */}
-            </Container>
-        </footer>
-  )
-}
+                {info.map((i) => (
+                  <Link
+                    href={i.link}
+                    key={i.id}
+                    className="text-sm text-white/[0.5] hover:text-white cursor-pointer"
+                  >
+                    {i.text}
+                  </Link>
+                ))}
+              </div>
 
-export default Footer
+              <div className="flex flex-col gap-3">
+                <div className="font-oswald font-medium uppercase text-sm">
+                  More
+                </div>
+
+                {
+                more.map(m=>(
+                    <Link href={m.link} key={m.id} className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                  {m.text}
+                </Link>
+                    ))
+
+                }
+
+              </div>
+
+
+            </div>
+
+            <div className="flex md:flex-col flex-row gap-8">
+
+                <div className=" flex flex-col gap-3">
+                <div className="font-oswald font-medium uppercase text-sm">
+                  Contact Us
+                </div>
+                <div className="text-white/[0.5]">
+                    123 BookCafe St, City, Country
+                </div>
+                </div>
+                
+                <div className="flex flex-col gap-3">   
+                
+                <div className="font-oswald font-medium uppercase text-sm">
+                  Join Us
+                </div>
+              
+                <div className="text-white/[0.5] flex items-center gap-2">
+                    Join Us on <Link href="/" className="text-white/[0.8] underline underline-offset-4"> WhatsApp</Link>
+                </div>
+
+                </div>
+
+
+                </div>
+
+          </div>
+        </Container>
+
+        <Container className="mt-10">
+          <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center">
+            © 2024 BookCafe, Inc. All Rights Reserved
+          </div>
+        </Container>
+      </footer>
+
+   
+  );
+};
+
+export default Footer;
