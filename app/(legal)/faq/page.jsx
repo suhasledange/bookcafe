dfd55c34d339a2c38d02ff7c421f590ef05ced7c
suhasledange <suhasledange10@gmail.com>
2024-04-16 14:19:37 +0530

    fix

--- app/(legal)/faq/page.jsx
+++ app/(legal)/faq/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const Faq = () => {
   return (
     <div>
       faq
--- app/(legal)/guide/page.jsx
+++ app/(legal)/guide/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const Guide = () => {
   return (
     <div>
       guide
