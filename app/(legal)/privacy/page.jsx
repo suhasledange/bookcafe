77f080de7309da925fb9775a2e7cb1ba903eaf42
suhasledange <suhasledange10@gmail.com>
2024-04-21 12:52:49 +0530

    legal

--- app/(legal)/privacy/page.jsx
+++ app/(legal)/privacy/page.jsx
@@ -1,10 +1,134 @@
+import Container from "@/app/components/Container";
 
 const Privacy = () => {
   return (
-    <div>
-      Privacy policy
-    </div>
-  )
-}
+    <Container className=" max-w-screen-xl py-10">
+      <div className="space-y-8 text-justify">
+        <h1 className="text-xl font-bold uppercase">Privacy Policy</h1>
 
-export default Privacy
+        <div className="text-gray-700 space-y-5">
+          <div className="">
+            <h2 className="font-semibold text-lg mb-1 text-black">
+              User Information
+            </h2>
+            <p className="">
+              To avail certain services on bookcafee.vercel.app, users are
+              required to provide certain information for the registration
+              process namely:
+            </p>
+            <p className="text-black"> Email address</p>
+          </div>
+
+          <div className="">
+            <h2 className="mb-1 font-semibold text-black text-lg">
+              Privacy Policy for BookCafe
+            </h2>
+            <p className="mb-2">
+              At BookCafe, accessible at{" "}
+              <a href="https://bookcafee.vercel.app/">
+                https://bookcafee.vercel.app/
+              </a>
+              , one of our main priorities is the privacy of our visitors. This
+              Privacy Policy document contains types of information that are
+              collected and recorded by BookCafe and how we use it.
+            </p>
+            <p>
+              If you have additional questions or require more information about
+              our Privacy Policy, do not hesitate to{" "}
+              <a href="contact.html">contact us</a>.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">
+              Privacy Policies
+            </h2>
+            <p className="mb-2">
+              You may consult this list to find the Privacy Policy for each of
+              the advertising partners of BookCafe. Our Privacy Policy was
+              created with the help of the Privacy Policy Generator and the
+              Privacy Policy Generator Online.
+            </p>
+            <p className="mb-2">
+              Third-party ad servers or ad networks uses technologies like
+              cookies, JavaScript, or Web Beacons that are used in their
+              respective advertisements and links that appear on BookCafe ,
+              which are sent directly to users’ browser. They automatically
+              receive your IP address when this occurs. These technologies are
+              used to measure the effectiveness of their advertising campaigns
+              and/or to personalize the advertising content that you see on
+              websites that you visit.
+            </p>
+            <p>
+              Note that BookCafe has no access to or control over these cookies
+              that are used by third-party advertisers.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">
+              Third-Party Privacy Policies
+            </h2>
+
+            <p className="mb-2">
+              BookCafe’s Privacy Policy does not apply to other advertisers or
+              websites. Thus, we are advising you to consult the respective
+              Privacy Policies of these third-party ad servers for more detailed
+              information. It may include their practices and instructions about
+              how to opt-out of certain options. You may find a complete list of
+              these Privacy Policies and their links here: Privacy Policy Links
+            </p>
+
+            <p>
+              You can choose to disable cookies through your individual browser
+              options. To know more detailed information about cookie management
+              with specific web browsers, it can be found at the browsers’
+              respective websites. What Are Cookies?
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">
+              Children’s Information
+            </h2>
+
+            <p className="mb-2">
+              Another part of our priority is adding protection for children
+              while using the internet. We encourage parents and guardians to
+              observe, participate in, and/or monitor and guide their online
+              activity BookCafe does not knowingly collect any Personal
+              Identifiable Information from children under the age of 13. If you
+              think that your child provided this kind of information on our
+              website, we strongly encourage you to contact us immediately and
+              we will do our best efforts to promptly remove such information
+              from our records.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">
+              Online Privacy Policy Only
+            </h2>
+
+            <p className="mb-2">
+            This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in BookCafe.This policy is not applicable to any information collected offline or via channels other than this website.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">
+                Consent
+            </h2>
+
+            <p className="mb-2">
+            By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Privacy;
--- app/(legal)/terms/page.jsx
+++ app/(legal)/terms/page.jsx
@@ -1,10 +1,132 @@
+import Container from "@/app/components/Container";
 
 const Terms = () => {
   return (
-    <div>
-      terms
-    </div>
-  )
-}
+    <Container className=" max-w-screen-xl py-10">
+      <div className="space-y-8 text-justify">
+        <h1 className="text-xl font-bold uppercase">Terms And Conditions</h1>
 
-export default Terms
+        <div className="text-gray-700 space-y-7">
+          <div>
+
+            <p className="mb-2">
+              “A half-read book is a half-finished love affair.”
+            </p>
+
+            <p className="mb-4">― David Mitchell, Cloud Atlas  </p>
+
+            <p className="mb-4">
+            The terms “We” / “Us” / “Our”/” Company” individually and collectively refer to BookCafe  and the terms “Visitor” User” refers to the users. 
+            </p>
+
+            <p>This page states the Terms and Conditions under which you (Visitor) may visit/order at BookCafe  a product of  Please read this page carefully. If you do not accept the Terms and Conditions stated here, we would request you to exit this site. The business, any of its business divisions and/or its subsidiaries, associate companies or subsidiaries to subsidiaries or such other investment companies (in India or abroad) reserve their respective rights to revise these Terms and Conditions at any time by updating this posting. You should visit this page periodically to re-appraise yourself of the Terms and Conditions because they are binding on all users of this Website.</p>
+
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">
+              Use Of Content
+            </h2>
+
+            <p className="mb-2">
+              All logos, brands, marks headings, labels, names, signatures,
+              numerals, shapes, or any combinations thereof, appearing in this
+              site, except as otherwise noted, are properties either owned or
+              used under license, by the business and/or its associate entities
+              who feature on this Website. The use of these properties or any
+              other content on this site, except as provided in these terms and
+              conditions or the site content, is strictly prohibited.
+            </p>
+          </div>
+
+          <div className="space-y-4">
+            <h2 className="font-semibold mb-2 capitalize text-black text-lg">
+              Acceptable Website Use
+            </h2>
+
+            <div className="space-y-3">
+              <h3 className="font-semibold">(A) Security Rules</h3>
+              <p className="mb-2">
+                Visitors are prohibited from violating or attempting to violate
+                the security of the Web site, including, without limitation,
+              </p>
+
+              <p className="mb-2">
+                Accessing data not intended for such user or logging into a
+                server or account which the user is not authorized to access
+                .Attempting to probe, scan or test the vulnerability of a system
+                or network or to breach security or authentication measures
+                without proper authorization
+              </p>
+              <p className="mb-2">
+                Attempting to interfere with service to any user, host, or
+                network, including, without limitation, via means of submitting
+                a virus or “Trojan horse” to the Website, overloading,
+                “flooding”, “mailbombing” or “crashing”, or Sending unsolicited
+                electronic mail, including promotions and/or advertising of
+                products or services. Violations of system or network security
+                may result in civil or criminal liability. The business and/or
+                its associate entities will have the right to investigate
+                occurrences that they suspect as involving such violations and
+                will have the right to involve and cooperate with, law
+                enforcement authorities in prosecuting users who are involved in
+                such violations.
+              </p>
+            </div>
+
+            <div className="space-y-3">
+              <h3 className="font-semibold">(B) General Rules </h3>
+              <p className="mb-2">
+                Visitors may not use the Web Site to transmit, distribute, store
+                or destroy material.
+              </p>
+
+              <p className="mb-2">
+                That could constitute or encourage conduct that would be
+                considered a criminal offense or violate any applicable law or
+                regulation In a manner that will infringe the copyright,
+                trademark, trade secret, or other intellectual property rights
+                of others or violate the privacy or publicity of other personal
+                rights of others, or (c) that is libellous, defamatory,
+                pornographic, profane, obscene, threatening, abusive or hateful.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">
+              Disclaimer Of Consequential Damages
+            </h2>
+
+            <p className="mb-2">
+              In no event shall Company or any parties, organisations or
+              entities associated with the corporate brand name us or otherwise,
+              mentioned at this Website be liable for any damages whatsoever
+              (including, without limitations, incidental and consequential
+              damages, lost profits, or damage to computer hardware or loss of
+              data information or business interruption) resulting from the use
+              or inability to use the Website and the Website material, whether
+              based on warranty, contract, tort, or any other legal theory, and
+              whether or not, such organisation or entities were advised of the
+              possibility of such damages.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold mb-1 text-black text-lg">Links</h2>
+
+            <p className="mb-2">
+              BookCafe has not reviewed all of the sites linked to its Internet
+              web site and is not responsible for the contents of any such
+              linked site. The inclusion of any link does not imply endorsement
+              by Bookcafe of the site. Use of any such linked web site is at the
+              user’s own risk.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Terms;
